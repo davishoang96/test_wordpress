eeb0b618793be85546e505bfa2395285af0368a3
Hoang Viet <vox@Voxs-MacBook-Pro.local>
2016-04-24 02:31:44 +0700

    new branch with new feature

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 
 <?php get_header(); ?>
     <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1 id="intro">The Mockup</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    <div class="container-fluid" id="intro">
+      <div class="row">
+        <div class="text-center">
+          <div class="col-md-12">
+            <h1>Interstellar</h1>
+            <p>
+              This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
+            </p>
+            <button type="button" class="btn btn-success" name="button">Press here</button>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="container">
+    <div class="container" id="all">
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-2">
@@ -22,14 +28,25 @@
 							if(have_posts()){
 								while(have_posts()){
 									the_post();
-									?>
+									/*?>
 									<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 									<label for=""><?php the_time( get_option( 'date_format g:i ' ) ); ?></label>
 									<p>
 										<?php the_excerpt(); ?>
 									</p>
 									<hr>
-									<?php
+									<?php*/
+                  ?>
+                  <div class="col-md-4">
+                    <div class="thumbnail">
+                      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                      <label for=""><?php the_time( get_option( 'date_format g:i ' ) ); ?></label>
+    									<p>
+    										<?php the_excerpt(); ?>
+    									</p>
+                    </div>
+                  </div>
+                  <?php
 								}
 							} else {
 								echo "<h1>No content found</h1>";
