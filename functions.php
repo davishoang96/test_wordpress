31f398fb422d4dfb40dffb4f8edc22f03a0110d0
Davis Hoang <davishoang96@gmail.com>
2016-04-25 09:47:41 +0700

    add some animations to page

--- functions.php
+++ functions.php
@@ -8,7 +8,12 @@
   function mycss_js_script(){
     wp_enqueue_script('bootstrap_js', get_template_directory_uri() . "/js/bootstrap.min.js" , array('jquery'), '', true);
   }
-  add_action('wp_enqueue_script', 'mycss_js_script');
+  add_action('wp_enqueue_scripts', 'mycss_js_script');
+
+  function main_jquery(){
+    wp_enqueue_script('main_jquery', get_template_directory_uri() . "/js/jquery-2.2.3.min.js" , array('jquery'), '', true);
+  }
+  add_action('wp_enqueue_scripts', 'main_jquery');
 
   function create_widget($name, $id, $description){
     register_sidebar(array(
@@ -17,6 +22,28 @@
       'description' => $description,
     ));
   }
+
+  #//////////////////////////////////////////
+  function stellar_js(){
+    wp_enqueue_script('stellar_js', get_template_directory_uri() . "/js/jquery.stellar.min.js", array('jquery'), '', true);
+  }
+  add_action("wp_enqueue_scripts", 'stellar_js');
+
+  function activate_stellar(){
+      wp_enqueue_script('stellar_js', get_template_directory_uri() . "/js/activate_stellar.js", array('jquery'), '', true);
+  }
+  add_action("wp_enqueue_scripts", 'activate_stellar');
+  #//////////////////////////////////////////
+
+  function css3_animate(){
+    wp_enqueue_style('css3Animate', get_template_directory_uri() . "/css/animations.css");
+  }
+  add_action("wp_enqueue_scripts", 'css3_animate');
+
+  function css3_animate_js(){
+    wp_enqueue_script('css3_animate_js', get_template_directory_uri() . "/js/css3-animate-it.js", array('jquery'), '', true);
+  }
+  add_action("wp_enqueue_scripts", 'css3_animate_js');
 
   create_widget('Front Left Block', 'front-left-block', 'This block is on the front left side of the page');
   create_widget('Front Right Block', 'front-right-block', 'This block is on the front right side of the page');
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 
 <?php get_header(); ?>
     <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="container-fluid" id="intro">
+    <div class="container-fluid" id="intro" data-stellar-background-ratio="0.5">
       <div class="row">
         <div class="text-center">
           <div class="col-md-12">
-            <h1>Interstellar</h1>
+            <div class="animatedParent">
+              <h1 class="animated bounceIn">Interstellar</h1>
+            </div>
             <p>
               This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.
             </p>
