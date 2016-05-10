d4d5c583745d7368cd470ebbf95ab239cdf31c74
Vox <vox@Vox.local>
2016-05-10 19:59:11 +0700

    Remove Comment

--- footer.php
+++ footer.php
@@ -3,10 +3,6 @@
 </footer>
 </div> <!-- /container -->
 
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
 <?php wp_footer(); ?>
 </body>
 </html>
