c9c520cded075daeccbc50bba93955fb33f3673e
Vox <vox@Vox.local>
2016-05-10 20:02:26 +0700

    Branch Test-Site

--- footer.php
+++ footer.php
@@ -2,7 +2,11 @@
   <p>&copy; 2015 Company, Inc.</p>
 </footer>
 </div> <!-- /container -->
-
+<div class="container">
+  <div class="row">
+    <h1>oaijsdoij</h1>
+  </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
